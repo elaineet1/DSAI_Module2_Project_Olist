f1426ad62de672d7e457a8ed341105951644e6f4
ElaineT <elaine.et1@gmail.com>
2026-03-08 16:32:40 +0800

    Add derived columns to payment and review fact models

--- olist_dbt/models/marts/facts/fact_order_payments.sql
+++ olist_dbt/models/marts/facts/fact_order_payments.sql
@@ -5,6 +5,11 @@ select
     p.payment_type,
     p.payment_installments,
     p.payment_value,
+    case
+        when p.payment_installments = 1 then 'Single Payment'
+        when p.payment_installments between 2 and 3 then 'Short Installments'
+        else 'Long Installments'
+    end as installment_group,
     o.order_purchase_timestamp
 from {{ ref('stg_olist_order_payments') }} p
 left join {{ ref('stg_olist_orders') }} o
--- olist_dbt/models/marts/facts/fact_order_reviews.sql
+++ olist_dbt/models/marts/facts/fact_order_reviews.sql
@@ -3,6 +3,11 @@ select
     r.order_id,
     o.customer_id,
     r.review_score,
+    case
+        when r.review_score >= 4 then 'Positive'
+        when r.review_score = 3 then 'Neutral'
+        else 'Negative'
+    end as review_sentiment,
     r.review_comment_title,
     r.review_comment_message,
     r.review_creation_date,
